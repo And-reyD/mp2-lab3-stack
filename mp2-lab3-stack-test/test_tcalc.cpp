018d56e1a81d86e0ca502446dbc9c1e182ca70ae
Andrey Davydov <adnreyda@yandex.ru>
2021-12-20 17:30:36 +0300

    Tests added

--- mp2-lab3-stack-test/test_tcalc.cpp
+++ mp2-lab3-stack-test/test_tcalc.cpp
@@ -1,12 +1,12 @@
 #include "pch.h"
 #include "..\mp2-lab3-stack\TCalc.h"
 
-TEST(TLCalc, can_create_calculator) {
-	ASSERT_NO_THROW(TLCalc c);
+TEST(TCalc, can_create_calculator) {
+	ASSERT_NO_THROW(TCalc c);
 }
 
-TEST(TLCalc, can_set_and_get_expression) {
-	TLCalc c;
+TEST(TCalc, can_set_and_get_expression) {
+	TCalc c;
 	string expr = "3.2 + 3";
 	string result;
 
@@ -16,58 +16,58 @@ TEST(TLCalc, can_set_and_get_expression) {
 	EXPECT_EQ(result, expr);
 }
 
-TEST(TLCalc, can_check_brackets) {
-	TLCalc c;
+TEST(TCalc, can_check_brackets) {
+	TCalc c;
 	c.set_expr("3 + 3");
 	ASSERT_NO_THROW(c.check_expr());
 }
 
-TEST(TLCalc, can_detect_correct_brackets) {
-	TLCalc c;
+TEST(TCalc, can_detect_correct_brackets) {
+	TCalc c;
 	c.set_expr("(3)");
 	ASSERT_TRUE(c.check_expr());
 }
 
-TEST(TLCalc, can_detect_incorrect_brackets) {
-	TLCalc c;
+TEST(TCalc, can_detect_incorrect_brackets) {
+	TCalc c;
 	c.set_expr("3)(");
 	ASSERT_FALSE(c.check_expr());
 }
 
-TEST(TLCalc, can_convert_to_postfix_1) {
-	TLCalc c;
+TEST(TCalc, can_convert_to_postfix_1) {
+	TCalc c;
 	c.set_expr("3");
 
 	ASSERT_NO_THROW(c.to_pstfix());
 	EXPECT_EQ(c.get_pstfix(), "3");
 }
 
-TEST(TLCalc, can_convert_to_postfix_2) {
-	TLCalc c;
+TEST(TCalc, can_convert_to_postfix_2) {
+	TCalc c;
 	c.set_expr("1.3 + 3");
 
 	ASSERT_NO_THROW(c.to_pstfix());
 	EXPECT_EQ(c.get_pstfix(), "1.3 3 +");
 }
 
-TEST(TLCalc, can_convert_to_postfix_3) {
-	TLCalc c;
+TEST(TCalc, can_convert_to_postfix_3) {
+	TCalc c;
 	c.set_expr("(1 + 3) * 3.5");
 
 	ASSERT_NO_THROW(c.to_pstfix());
 	EXPECT_EQ(c.get_pstfix(), "1 3 + 3.5 *");
 }
 
-TEST(TLCalc, can_convert_to_postfix_4) {
-	TLCalc c;
+TEST(TCalc, can_convert_to_postfix_4) {
+	TCalc c;
 	c.set_expr("6 + 3.3^(4 / 2) - 4");
 
 	ASSERT_NO_THROW(c.to_pstfix());
 	EXPECT_EQ(c.get_pstfix(), "6 3.3 4 2 / ^ + 4 -");
 }
 
-TEST(TLCalc, can_add) {
-	TLCalc c;
+TEST(TCalc, can_add) {
+	TCalc c;
 	c.set_expr("3.5 + 3");
 	c.to_pstfix();
 
@@ -75,8 +75,8 @@ TEST(TLCalc, can_add) {
 	EXPECT_EQ(c.calc(), 3.5 + 3);
 }
 
-TEST(TLCalc, can_substract) {
-	TLCalc c;
+TEST(TCalc, can_substract) {
+	TCalc c;
 	c.set_expr("3 - 5.0");
 	c.to_pstfix();
 
@@ -84,8 +84,8 @@ TEST(TLCalc, can_substract) {
 	EXPECT_EQ(c.calc(), 3- 5.0);
 }
 
-TEST(TLCalc, can_multiply) {
-	TLCalc c;
+TEST(TCalc, can_multiply) {
+	TCalc c;
 	c.set_expr("3 * 5");
 	c.to_pstfix();
 
@@ -93,8 +93,8 @@ TEST(TLCalc, can_multiply) {
 	EXPECT_EQ(c.calc(), 3 * 5);
 }
 
-TEST(TLCalc, can_divide) {
-	TLCalc c;
+TEST(TCalc, can_divide) {
+	TCalc c;
 	c.set_expr("9 / 3");
 	c.to_pstfix();
 
@@ -102,8 +102,8 @@ TEST(TLCalc, can_divide) {
 	EXPECT_EQ(c.calc(), 9 / 3);
 }
 
-TEST(TLCalc, can_raise_to_the_power) {
-	TLCalc c;
+TEST(TCalc, can_raise_to_the_power) {
+	TCalc c;
 	c.set_expr("3^2");
 	c.to_pstfix();
 
@@ -111,8 +111,8 @@ TEST(TLCalc, can_raise_to_the_power) {
 	EXPECT_EQ(c.calc(), pow(3, 2));
 }
 
-TEST(TLCalc, can_calculate_complex_expression_1) {
-	TLCalc c;
+TEST(TCalc, can_calculate_complex_expression_1) {
+	TCalc c;
 	c.set_expr("1 - 2.5 + 3 * 3^2");
 	c.to_pstfix();
 
@@ -120,8 +120,8 @@ TEST(TLCalc, can_calculate_complex_expression_1) {
 	EXPECT_EQ(c.calc(), 1 - 2.5 + 3 * pow(3, 2));
 }
 
-TEST(TLCalc, can_calculate_complex_expression_2) {
-	TLCalc c;
+TEST(TCalc, can_calculate_complex_expression_2) {
+	TCalc c;
 	c.set_expr("((2 + 10) / 3 * 4^2) - 2.5 + 4");
 	c.to_pstfix();
 
@@ -129,8 +129,8 @@ TEST(TLCalc, can_calculate_complex_expression_2) {
 	EXPECT_EQ(c.calc(), ((2 + 10) / 3 * pow(4, 2)) - 2.5 + 4);
 }
 
-TEST(TLCalc, can_calculate_complex_expression_3) {
-	TLCalc c;
+TEST(TCalc, can_calculate_complex_expression_3) {
+	TCalc c;
 	c.set_expr("((4 + 2) / (6 - 7.0 / 2.0) + 2 - 4 ^ (5 - 2))");
 	c.to_pstfix();
 
