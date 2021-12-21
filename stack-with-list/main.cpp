d592edb23b024328763e4f77629b9c694fd95ec8
Andrey Davydov <adnreyda@yandex.ru>
2021-12-21 14:56:55 +0300

    Fixed mistakes

--- stack-with-list/main.cpp
+++ stack-with-list/main.cpp
@@ -3,12 +3,66 @@
 #include <clocale>
 #include <string>
 
+#include "TLStack.h"
 #include "TLCalc.h"
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "russian");
     setlocale(LC_NUMERIC, "eng");
+
+    TLStack<int> st;
+    cout << "st: " << st << "\nEmpty = " << st.empty() << "\n\n";
+
+    for (int i = 1; i <= 3; i++) {
+        cout << i << " pushed:\n";
+        st.push(i);
+        cout << st << "\nEmpty = " << st.empty() << "\n\n";
+    }
+    cout << "\n";
+
+    cout << "Assign st to st2 and clear st:\n";
+    TLStack<int> st2 = st;
+    st.clear();
+    cout << "st: " << st << "\nEmpty = " << st.empty() << "\n\n";
+    cout << "st2: " << st2 << "\nEmpty = " << st2.empty() << "\n\n";
+
+
+    cout << "We can't pop from st again:\n";
+    try {
+        st.pop();
+    }
+    catch (...) {
+        cout << "Exception\n\n";
+    }
+
+    cout << "Then we continue with st2.\n\n";
+
+    for (int i = 1; i <= 3; i++) {
+        cout << st2.pop() << " popped:\n";
+        cout << st2 << "\nEmpty = " << st2.empty() << "\n\n";
+    }
+    cout << "\n";
+
+
+    for (int i = 4; i <= 6; i++) {
+        cout << i << " pushed:\n";
+        st2.push(i);
+        cout << st2 << "\nEmpty = " << st2.empty() << "\n\n";
+    }
+
+    cout << "Other tests:\n\n";
+    TLStack<int> st3(st2);
+
+    cout << "st2: " << st2 << "\nEmpty = " << st2.empty() << "\n\n";
+    cout << "st3(st2): " << st3 << "\nEmpty = " << st3.empty() << "\n\n";
+
+    cout << "(st3 == st2) = " << (st3 == st2) << "\n\n";
+
+    cout << st2.pop() << " popped from st2:\n";
+    cout << "st2: " << st2 << "\nEmpty = " << st2.empty() << "\n\n";
+
+    cout << "(st3 == st2) = " << (st3 == st2) << "\n";
 
     TLCalc calculator;
     string line;
